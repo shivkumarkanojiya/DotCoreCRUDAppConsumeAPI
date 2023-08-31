dde13e8f8de9c60dc26349a0c84bb02760c7c157
shivkumarkanojiya <132676192+shivkumarkanojiya@users.noreply.github.com>
2023-08-31 18:49:52 +0530

    Initial commit

---
+++ CRUDAppUsingAspNetCoreWebAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDAppUsingAspNetCoreWebAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUDAppUsingAspNetCoreWebAPI/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAppUsingAspNetCoreWebAPI.Models
+{
+
+
+    public class Student
+    {
+        [Display(Name ="ID")]
+        public int id { get; set; }
+        [Display(Name = "Student Name")]
+        [Required]
+        public string? studentName { get; set; }
+        [Display(Name = "Gender")]
+        [Required]
+        public string? studentGender { get; set; }
+        [Display(Name = "Age")]
+        [Required]
+        public int age { get; set; }
+        [Display(Name = "Standard")]
+        [Required]
+        public int standard { get; set; }
+        [Display(Name = "Father Name")]
+        [Required]
+        public string? fatherName { get; set; }
+    }
+
+}
